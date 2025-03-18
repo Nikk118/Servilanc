bdd5d8757a52e79717b8f1dcdeafc0490b663ea7
Bariharsh <144584767+Bariharsh@users.noreply.github.com>
2025-03-18 16:51:29 +0530

    deleted sms

--- backend/src/router/sms.router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { sendSms } from '../controller/sms.controller.js';
-
-const router = express.Router();
-
-
-router.post('/send-sms', sendSms);
-
-export default router;
